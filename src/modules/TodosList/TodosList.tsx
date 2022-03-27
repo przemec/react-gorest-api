74611a147f5abb8f53970a0a7230caa7cb184ceb
Przem ThinkBad <przemek-szczepaniak@o2.pl>
2022-03-27 22:58:46 +0200

    added comments describing functionality of Todos module

--- src/modules/TodosList/TodosList.tsx
+++ src/modules/TodosList/TodosList.tsx
@@ -10,10 +10,14 @@ import SearchBar from "../../components/SearchBar";
 import TodoResult from "../../components/TodoResult";
 
 const TodosList: React.FC<C.TodosProps> = ({ todos, addTodos, requestPage, updatePage }) => {
+  //string that is used to filter rendered results by their main value
   const [searchFilter, setSearchFilter] = useState("");
+  //bool value that indicates if the data from API was fully fetched (that all response pages were fetched)
   const [isLoadingDone, setLoadDone] = useState(false);
+  //element that intersection observer will observe to fetch data if the element is visible
   const [observedElement, setElement] = useState<HTMLElement | null>(null);
 
+  //intersection observer - created with useRef, so with each render of the component it will not be created new instance of observer
   const intersectionObserver = useRef(
     new IntersectionObserver(
       (entries) => {
@@ -24,6 +28,9 @@ const TodosList: React.FC<C.TodosProps> = ({ todos, addTodos, requestPage, updat
     )
   );
 
+  //loadTodos wraps fetch with useCallback so it's refreshed when requestPage changes
+  //it's used later when loader function wraps it with useRef, and loader.current() is used in intersection observer*
+  //*so observer always calls loadTodos function with proper requestPage value
   const loadTodos = useCallback(() => {
     fetch(`https://gorest.co.in/public/v1/todos?page=${requestPage}`, { method: "GET" })
       .then((res) => res.json())
@@ -40,18 +47,23 @@ const TodosList: React.FC<C.TodosProps> = ({ todos, addTodos, requestPage, updat
   }, [loadTodos]);
 
   useEffect(() => {
+    //select current instance of observed element and of intersection observer
     const currentElement = observedElement;
     const currentObserver = intersectionObserver.current;
 
+    //if observed element is not rendered, return
     if (!currentElement) return;
 
+    //if observed element is rendered, observe it
     currentObserver.observe(currentElement);
+    //cleanup function to unobserve element while it's being unmounted
     return () => {
       currentObserver.unobserve(currentElement);
     };
   }, [observedElement]);
 
   useEffect(() => {
+    //initial fetch of data if the state's list is empty
     if (todos.length !== 0) return;
     loadTodos();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -64,6 +76,7 @@ const TodosList: React.FC<C.TodosProps> = ({ todos, addTodos, requestPage, updat
         {todos.length === 0 ? (
           <h1>Loading initial records...</h1>
         ) : (
+          //map data array and then filter rendered elements with value from search bar
           todos
             .map((todo, index: number) => <TodoResult key={index} data={todo} />)
             .filter((e) => e.props.data.title.toLowerCase().includes(searchFilter.toLowerCase()))
